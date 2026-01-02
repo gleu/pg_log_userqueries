cac11f4644fa3fb2ec7ae88455d8b5893e3cfb51
Guillaume Lelarge <guillaume@lelarge.info>
2026-01-02 12:11:05 +0100

    Update copyright year

--- pg_log_userqueries.c
+++ pg_log_userqueries.c
@@ -4,7 +4,7 @@
  *		Log statement according to the user.
  *
  *
- * Copyright (c) 2011-2025, Guillaume Lelarge (Dalibo),
+ * Copyright (c) 2011-2026, Guillaume Lelarge (Dalibo),
  * guillaume.lelarge@dalibo.com
  *
  *-------------------------------------------------------------------------
